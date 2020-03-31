62eb7f7b9a3c3ef37ac6f2ada6cb7a7601af20f1
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-03-31 19:54:56 -0300

    adding code to retrive external links

--- chapter3.py
+++ chapter3.py
@@ -18,35 +18,30 @@ def getInternalLinks(soup, includeUrl):
 
     # Finds all links that begin with a "/"
     for link in soup.findAll("a", href=re.compile("^(/|.*"+includeUrl+")")):
+
         if link.attrs['href'] is not None:
             if link.attrs['href'] not in internalLinks:
                 internalLinks.append(link.attrs['href'])
     return internalLinks
 
 
-def getLinks(articleLink):
+# Retrieves a list of all external links found on a page
+def getExternalLinks(soup, excludeUrl):
+    externalLinks = []
 
-    soup = BeautifulSoup(
-        urlopen("https://en.wikipedia.org" + articleLink),
-        features="html.parser")
+    # Finds all links that start with "http" or "www" that do
+    # not contain the current URL
+    for link in soup.findAll("a",
+                href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
 
-    try:
-        print(soup.body.h1.get_text())
-        print(soup.findAll(attrs={"id" : "mw-content-text"}).findAll("p")[0])
-        print(soup.find(id="ca-edit").find("span").find("a").attrs['href'])
-    except AttributeError as e:
-        print("there is something missing in this page")
-
-        for links in soup.findAll("a", {"href": re.compile("^(/wiki/)")}):
-            if links.attrs["href"] not in pages:
-                newPage = links.attrs['href']
-                print("-"*60 + "\n" + newPage)
-                pages.add(newPage)
-                getLinks(newPage)
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
 
 
 if __name__ == "__main__":
-    getLinks("")
+    pass
 
 
 end = time.clock()
