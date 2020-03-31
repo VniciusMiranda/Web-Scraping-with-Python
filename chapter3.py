44b55a6f9ebc58c59b641b38c26b8eedef8c0a94
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-03-31 12:26:54 -0300

    starting a new exercise

--- chapter3.py
+++ chapter3.py
@@ -8,52 +8,21 @@ import random
 import threading
 
 
-_FINISH = False
-
-random.seed(datetime.datetime.now())
-
 start = time.clock()
-
-
-def checkQuit():
-    global _FINISH
-
-    while not _FINISH:
-        ip = input()
-        if ip == "q":
-            _FINISH = True
+pages = set()
 
 
 def getLinks(articleLink):
-    try:
-        html = urlopen("https://en.wikipedia.org/" + articleLink)
-    except HTTPError as e:
-        print(e)
-        return None
 
-    try:
-        soup = BeautifulSoup(html, features="html.parser")
-    except AttributeError as e:
-        print(e)
-        return None
+    soup = BeautifulSoup(
+        urlopen("https://en.wikipedia.org"),
+        features="html.parser")
 
-    return soup.find("div", {"id": "bodyContent"}).findAll("a", {"href": re.compile("^(/wiki/)((?!:).)*$")})
+    soup.find()
 
-
-exitThread = threading.Thread(name="exitThread", target=checkQuit)
-exitThread.start()
 
 if __name__ == "__main__":
-
-    links = getLinks("/wiki/Library_Genesis")
-
-    while not _FINISH:
-        if not len(links) > 0:
-            break
-        print(f"len(links) = {len(links)}")
-        newArticle = links[random.randint(0, len(links) - 1)].attrs["href"]
-        print(newArticle)
-        links = getLinks(newArticle)
+    getLinks("")
 
 
 end = time.clock()
