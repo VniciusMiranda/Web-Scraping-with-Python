9158da2867552866309f5d5849ca0f9248ffea38
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-03-31 01:25:38 -0300

    chapter 3 code

--- scrapertest.py
+++ scrapertest.py
@@ -1,33 +1,63 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import re
+import time
+import datetime
+import random
+import threading
 
 
-def get_title(url):
+_FINISH = False
+
+random.seed(datetime.datetime.now())
+
+start = time.clock()
+
+
+def checkQuit():
+    global _FINISH
+
+    while not _FINISH:
+        ip = input()
+        if ip == "q":
+            _FINISH = True
+
+
+def getLinks(articleLink):
     try:
-        html = urlopen(url)
+        html = urlopen("https://en.wikipedia.org/" + articleLink)
     except HTTPError as e:
         print(e)
         return None
 
     try:
-        soup = BeautifulSoup(html.read(), features="html.parser")
-        title = soup.body
+        soup = BeautifulSoup(html, features="html.parser")
     except AttributeError as e:
+        print(e)
         return None
 
-    return title
+    return soup.find("div", {"id": "bodyContent"}).findAll("a", {"href": re.compile("^(/wiki/)((?!:).)*$")})
 
+
+exitThread = threading.Thread(name="exitThread", target=checkQuit)
+exitThread.start()
 
 if __name__ == "__main__":
 
-    url = "http://www.pythonscraping.com/pages/page3.html"
+    links = getLinks("/wiki/Library_Genesis")
 
-    title = get_title(url)
+    while not _FINISH:
+        if not len(links) > 0:
+            break
+        print(f"len(links) = {len(links)}")
+        newArticle = links[random.randint(0, len(links) - 1)].attrs["href"]
+        print(newArticle)
+        links = getLinks(newArticle)
 
-    if title is None:
-        print("title could not be found")
 
-    else:
+end = time.clock()
 
-        print(title.get_text())
+print("=" * 60)
+
+print(f"the program took: {end - start} seconds to execute...")
