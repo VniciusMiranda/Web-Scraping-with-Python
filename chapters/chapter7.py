d6ed4c472e5f082982abf9262f5719e62fcdeb39
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-05-21 23:48:01 -0300

    adding a function to clean the code

--- chapters/chapter7.py
+++ chapters/chapter7.py
@@ -18,6 +18,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError
+import re
+import string
 
 def n_grams(input, n):
     '''
@@ -25,16 +27,29 @@ def n_grams(input, n):
     :param n: number of words
     :return: all n-grams of the text
     '''
-    input = input.split(" ")
-
     output = list()
-    for i in range(len(input)):
+    for i in range(len(input) + n - 1):
         output.append(input[i: i + n])
 
     return output
 
 
+def clean_input(input):
+    input = re.sub("\n+", " ", input)
+    input = re.sub(" +", " ", input)
+    input = re.sub("\[[0-9]*\]", "", input)
+    input = bytes(input, "UTF-8")
+    input = input.decode("ascii", "ignore")
 
+    cleanInput = list()
+    print(type(input))
+    input = input.split(' ')
+    for item in input:
+        item = item.strip(string.punctuation)
+        if len(item) > 1 or (item.lower() == 'a' or item.lower() == 'i'):
+            cleanInput.append(item)
+
+    return cleanInput
 if __name__ == "__main__":
     try:
         html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
@@ -42,9 +57,8 @@ if __name__ == "__main__":
         print(error.getcode())
 
     soup = BeautifulSoup(html, features="html.parser")
-
     content = soup.find("div", {"id": "mw-content-text"}).get_text()
+    content = clean_input(content)
     ngrams = n_grams(content, 2)
-
     print(ngrams)
     print("number of n grams of 2 is: {0}".format(len(ngrams)))
